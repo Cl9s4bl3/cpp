a8ad492cb994737a78c61d7b8c11696f8fc31f7a
Sőrés Máté <126987836+Cl9s4bl3@users.noreply.github.com>
2025-02-12 09:03:06 +0100

    Added the abilty to remove items from a category

--- costly/src/category.cpp
+++ costly/src/category.cpp
@@ -39,8 +39,7 @@ void createCategory(){
     outFile.close();
 }
 
-void categoryEntryManager(){
-
+void addToCategory(){
     json data;
 
     std::string entry_name;
@@ -56,13 +55,9 @@ void categoryEntryManager(){
     std::cout << "\nEnter the name of the category: ";
     std::getline(std::cin, cat_name);
 
-
-
     std::ifstream inFile(FILE);
     inFile >> data;
     inFile.close();
-
-    //There's an error that causes the text not to appear after entering an invalid entry
 
     if (!(data["expense"].contains(entry_name) || data["income"].contains(entry_name))){
         std::cout << "\nThis entry does not exist." << std::endl;
@@ -73,12 +68,82 @@ void categoryEntryManager(){
             return;
         } else {
             data["categories"][cat_name][entry_name] = {};
+            std::cout << "\nSuccessfully added '" << entry_name << "' to category '" << cat_name << "'." << std::endl;
         }
     }
 
     std::ofstream outFile(FILE);
     outFile << data.dump(4);
     outFile.close();
+}
+
+void removeFromCategory(){
+    json data;
+
+    std::string entry_name;
+    std::string cat_name;
+
+    overView();
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    std::cout << "\nEnter the name of the entry: ";
+    std::getline(std::cin, entry_name);
+
+    std::cout << "\nEnter the name of the category: ";
+    std::getline(std::cin, cat_name);
+
+    std::ifstream inFile(FILE);
+    inFile >> data;
+    inFile.close();
+
+    if (!(data["expense"].contains(entry_name) || data["income"].contains(entry_name))){
+        std::cout << "\nThis entry does not exist." << std::endl;
+        return;
+    } else {
+        if (!data["categories"].contains(cat_name)){
+            std::cout << "\nThe specified category does not exist." << std::endl;
+            return;
+        } else {
+            data["categories"][cat_name].erase(entry_name);
+            std::cout << "\nSuccessfully deleted '" << entry_name << "' from category '" << cat_name << "'." << std::endl;
+        }
+    }
+
+    std::ofstream outFile(FILE);
+    outFile << data.dump(4);
+    outFile.close();
+}
+
+void categoryEntryManager(){
+    int action;
+    std::cout << "\nAvailable actions:\n1) Add item to a category\n2) Remove item from a category\n3) Exit" << std::endl;
+    std::cout << "\nChoose an action: ";
+    std::cin >> action;
+
+    if (std::cin.fail()){
+        if (std::cin.fail()){
+            std::cout << "\nInvalid action!\n" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    } else {
+        switch (action) {
+            case 1:
+                addToCategory();
+                break;
+
+            case 2:
+                removeFromCategory();
+                break;
+            case 3:
+                return;
+
+            default:
+                std::cout << "Invalid action." << std::endl;
+        }
+    }
+
 }
 
 void removeCategory(){
@@ -112,7 +177,7 @@ void categoryManager(){
 
     while (true){
         int action;
-        std::cout << "\nAvailable actions:\n1) Create category\n2) Add to category\n3) Remove Category\n4) Exit" << std::endl;
+        std::cout << "\nAvailable actions:\n1) Create category\n2) Remove Category\n3) Add to or remove from category\n4) Exit" << std::endl;
         std::cout << "\nChoose an action: ";
         std::cin >> action;
 
@@ -128,11 +193,11 @@ void categoryManager(){
                 break;
 
             case 2:
-                categoryEntryManager();
+                removeCategory();
                 break;
 
             case 3:
-                removeCategory();
+                categoryEntryManager();
                 break;
 
             case 4:
